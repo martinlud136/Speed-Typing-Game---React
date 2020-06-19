caeb87f8f22425ced4187e35620b592eebf549c6
Martinlud <hectormartinluduena@gmail.com>
2020-06-19 10:59:11 -0300

    reset the game

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const START_TIME = 5
+
   const [text, setText] = useState("")
-  const [timeRemaining, setTimeRemaining] = useState(5)
+  const [timeRemaining, setTimeRemaining] = useState(START_TIME)
   const [isTimeRuning, setIsTimeRuning] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
 
   function handleChange(e){
     const {value} = e.target
@@ -17,13 +21,24 @@ function App() {
     return arrFiltered.length
   }
 
+  function startGame(){
+    setIsTimeRuning(true)
+    setTimeRemaining(START_TIME)
+    setText('')
+  }
+
+  function endGame(){
+    setIsTimeRuning(false)
+    setWordCount(countTotalWords(text))
+  }
+
   useEffect(()=>{
     if(timeRemaining > 0 && isTimeRuning){
       setTimeout(() =>{
         setTimeRemaining( prevTime => prevTime - 1)
       }, 1000)
-    }else if(isTimeRuning === 0){
-      setIsTimeRuning(false)
+    } else if( timeRemaining === 0 ) {
+      endGame()
     }
   },[timeRemaining, isTimeRuning])
 
@@ -36,8 +51,8 @@ function App() {
               onChange={handleChange}
             />
             <h4>Time remaining: {timeRemaining}</h4>
-            <button onClick={() => setIsTimeRuning(true)}>Start</button>
-            <h1>Word count: ???</h1>
+            <button onClick={startGame}>Start</button>
+            <h1>Word count: {wordCount} </h1>
         </div>
     </>
   );
